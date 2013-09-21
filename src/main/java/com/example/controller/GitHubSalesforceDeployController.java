9cba83da3b83a2c526706b8db331f174c1f8cb6a
Andrew Fawcett <afawcett@financialforce.com>
2013-09-22 00:36:42 +0100

    Added AJAX deploy monitoring

--- src/main/java/com/example/controller/GitHubSalesforceDeployController.java
+++ src/main/java/com/example/controller/GitHubSalesforceDeployController.java
@@ -5,14 +5,19 @@ import static org.eclipse.egit.github.core.client.IGitHubConstants.SEGMENT_REPOS
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.eclipse.egit.github.core.IRepositoryIdProvider;
 import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.RepositoryContents;
 import org.eclipse.egit.github.core.RepositoryId;
 import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.client.GitHubRequest;
@@ -21,10 +26,12 @@ import org.eclipse.egit.github.core.service.ContentsService;
 import org.eclipse.egit.github.core.service.RepositoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.force.sdk.connector.ForceServiceConnector;
-import com.sforce.soap.metadata.AsyncRequestState;
 import com.sforce.soap.metadata.AsyncResult;
 import com.sforce.soap.metadata.CodeCoverageWarning;
 import com.sforce.soap.metadata.DeployMessage;
@@ -34,7 +41,7 @@ import com.sforce.soap.metadata.MetadataConnection;
 import com.sforce.soap.metadata.RunTestFailure;
 import com.sforce.soap.metadata.RunTestsResult;
 
-// githubsfdeploy.herokuapp.com/sfdc/githubdeploy/financialforcedev/fflib-apex-common
+// https://githubsfdeploy.herokuapp.com/sfdc/githubdeploy/financialforcedev/fflib-apex-common
 
 // http://localhost:8080/sfdc/githubdeploy/financialforcedev/fflib-apex-common
 
@@ -42,33 +49,68 @@ import com.sforce.soap.metadata.RunTestsResult;
 @RequestMapping("/githubdeploy")
 public class GitHubSalesforceDeployController {
     
-    private static final long ONE_SECOND = 1000;
-    private static final int MAX_NUM_POLL_REQUESTS = 50;     
-	
-    @RequestMapping("/{owner}/{repo}")
-    public String listContents(@PathVariable("owner") String repoOwner, @PathVariable("repo") String repoName, Map<String, Object> map) throws Exception {
-        
+    @RequestMapping(method = RequestMethod.GET, value = "/{owner}/{repo}")
+    public String confirm(@PathVariable("owner") String repoOwner, @PathVariable("repo") String repoName, Map<String, Object> map) throws Exception 
+    {
+    	try
+    	{	    	
+    		// Repository name
+	    	RepositoryId repoId = RepositoryId.create(repoOwner, repoName);
+    		map.put("repositoryName", repoId.generateId());
+    		
+	    	// Display user info
+	    	map.put("userContext", new ForceServiceConnector(ForceServiceConnector.getThreadLocalConnectorConfig()).getConnection().getUserInfo());
+	    	
+	    	// Display repo info
+	    	GitHubClientOAuthServer client = new GitHubClientOAuthServer("c9172f8413daedef3f1f", "b53b2ecb62b7a328a6f6889edf7867426aedf4a2" );
+	    	map.put("repo", null);
+	    	map.put("githubcontents", null);
+	    	RepositoryService service = new RepositoryService(client);
+	    	map.put("repo", service.getRepository(repoId));
+	    	
+	    	// Retrieve repository contents applicable for deploy
+	    	RepositoryItem repositoryContainer = new RepositoryItem();
+	    	repositoryContainer.repositoryItems = new ArrayList<RepositoryItem>();
+	    	ContentsServiceEx contentService = new ContentsServiceEx(client);
+	    	processContents(contentService, repoId, contentService.getContents(repoId), repositoryContainer);
+	    	ObjectMapper mapper = new ObjectMapper();	    	
+	    	map.put("githubcontents", mapper.writeValueAsString(repositoryContainer));
+	    	
+    	}
+    	catch (Exception e)
+    	{
+    		// Handle error
+    		map.put("error", "Failed to retrive GitHub repository details : " + e.toString());
+    		e.printStackTrace();
+    	}
+    	return "githubdeploy";
+    }
+    
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.POST, value = "/{owner}/{repo}")
+    public String deploy(@PathVariable("owner") String repoOwner, @PathVariable("repo") String repoName, @RequestBody String repoContentsJson) throws Exception 
+    {
     	// Connect via oAuth client and secret to get greater request limits
     	GitHubClientOAuthServer client = 
     		new GitHubClientOAuthServer("c9172f8413daedef3f1f", "b53b2ecb62b7a328a6f6889edf7867426aedf4a2" );
-    	
-    	// Repo description
+   
+    	// Repository container (files to deploy)
+    	ObjectMapper mapper = new ObjectMapper();
+    	RepositoryItem repositoryContainer = (RepositoryItem) mapper.readValue(repoContentsJson, RepositoryItem.class);
+
+    	// Repository description
     	RepositoryService service = new RepositoryService(client);
     	RepositoryId repoId = RepositoryId.create(repoOwner, repoName);
-    	Repository repo = service.getRepository(repoId);    	
-        StringBuilder deploymentFiles = new StringBuilder();
-        deploymentFiles.append( "Repository:  " + repoOwner + "/" + repoName + "</br>");
-        deploymentFiles.append( "Description: " + repo.getDescription() + "</br>");
-        deploymentFiles.append( "Files:</br>");
+    	Repository repo = service.getRepository(repoId); 
         
         // Content service
         ContentsServiceEx contentService = new ContentsServiceEx(client);
+        ZipInputStream zipIS = contentService.getArchiveAsZip(repoId);
         
         // Download Zip and generate Metadata Deploy Zip!
         String salesforceDeployRoot = null;
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		ZipOutputStream zipOS = new ZipOutputStream(baos);
-        ZipInputStream zipIS = contentService.getArchiveAsZip(repoId);
 		while(true)
 		{
 			ZipEntry zipEntry = zipIS.getNextEntry();
@@ -86,7 +128,6 @@ public class GitHubSalesforceDeployController {
 					// Generate the Metadata zip entry name (from regex above?)
 					String metadataZipEntryName = zipEntry.getName().substring(salesforceDeployRoot.length());					
 					ZipEntry metadataZipEntry = new ZipEntry(metadataZipEntryName);
-					deploymentFiles.append(metadataZipEntryName + "</br>");					
 					zipOS.putNextEntry(metadataZipEntry);
 					// Copy bytes over from Github archive input stream to Metadata zip output stream
 					byte[] buffer = new byte[1024];
@@ -101,51 +142,72 @@ public class GitHubSalesforceDeployController {
 		zipOS.close();
 		if(salesforceDeployRoot==null)
 			throw new Exception("Did not find the /src folder");
-		map.put("deploymentFiles", deploymentFiles);
 				
 		// Connect to Salesforce Metadata API
         ForceServiceConnector connector = new ForceServiceConnector(ForceServiceConnector.getThreadLocalConnectorConfig());
         MetadataConnection metadataConnection = connector.getMetadataConnection();
+				
+		// Deploy to Salesforce
 		DeployOptions deployOptions = new DeployOptions();
 		deployOptions.setSinglePackage(true);
 		deployOptions.setPerformRetrieve(false);
 		deployOptions.setRollbackOnError(true);
-				
-		// Deploy and handle any errors
+		deployOptions.setRunAllTests(true);
 		AsyncResult asyncResult = metadataConnection.deploy(baos.toByteArray(), deployOptions);
-	    int poll = 0;
-        long waitTimeMilliSecs = ONE_SECOND;
-        StringBuilder deployProgress = new StringBuilder();
-        while (!asyncResult.isDone()) {
-            Thread.sleep(waitTimeMilliSecs);
-            // double the wait time for the next iteration
-            waitTimeMilliSecs *= 2;
-            if (poll++ > MAX_NUM_POLL_REQUESTS) {
-                throw new Exception("Request timed out. If this is a large set " +
-                        "of metadata components, check that the time allowed by " +
-                        "MAX_NUM_POLL_REQUESTS is sufficient.");
-            }
-            asyncResult = metadataConnection.checkStatus(
-                    new String[] {asyncResult.getId()})[0];
-            deployProgress.append("Status is: " + asyncResult.getState() + "</br>");
-        }
-        map.put("deployProgress", deployProgress);
-        
-        String resultErrorMessages = "";
-        if (asyncResult.getState() != AsyncRequestState.Completed) {
-        	resultErrorMessages = " msg: " + asyncResult.getMessage();
-        }
-        else {
-	        DeployResult result = metadataConnection.checkDeployStatus(asyncResult.getId());
-	        if (!result.isSuccess()) {
-	        	resultErrorMessages = printErrors(result);
-	        }
-        }
-        map.put("resultErrorMessages", resultErrorMessages);
-        
-    	return "githubdeploy";
+
+		// Given the client the AysncResult to poll for the result of the deploy
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.getSerializationConfig().addMixInAnnotations(AsyncResult.class, AsyncResultMixIn.class);
+		return objectMapper.writeValueAsString(asyncResult);
     }
 
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET, value = "/{owner}/{repo}/checkstatus/{asyncId}")
+    public String checkStatus(@PathVariable("asyncId") String asyncId) throws Exception 
+    {
+    	// Connect to Metadata API, check async status and return to client
+        ForceServiceConnector connector = new ForceServiceConnector(ForceServiceConnector.getThreadLocalConnectorConfig());
+        MetadataConnection metadataConnection = connector.getMetadataConnection();
+        AsyncResult asyncResult =  metadataConnection.checkStatus(new String[] { asyncId })[0];
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.getSerializationConfig().addMixInAnnotations(AsyncResult.class, AsyncResultMixIn.class);    	
+		return objectMapper.writeValueAsString(asyncResult);
+    }
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET, value = "/{owner}/{repo}/checkdeploy/{asyncId}")
+    public String checkDeploy(@PathVariable("asyncId") String asyncId) throws Exception 
+    {
+    	// Connect to Metadata API, check async status and return to client
+        ForceServiceConnector connector = new ForceServiceConnector(ForceServiceConnector.getThreadLocalConnectorConfig());
+        MetadataConnection metadataConnection = connector.getMetadataConnection();
+        DeployResult deployResult = metadataConnection.checkDeployStatus(asyncId);
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.writeValueAsString(printErrors(deployResult));
+    }
+
+    /**
+     * Used with the Jackson JSON library to exclude conflicting getters when serialising AsyncResult 
+     *   (see http://wiki.fasterxml.com/JacksonMixInAnnotations)
+     */
+    public abstract class AsyncResultMixIn
+    {
+    	@JsonIgnore abstract boolean isCheckOnly();
+    	@JsonIgnore abstract boolean isDone();
+    }
+       
+    /**
+     * Container to reflect repository structure
+     */
+    public static class RepositoryItem
+    {
+    	public RepositoryContents repositoryItem;
+    	public ArrayList<RepositoryItem> repositoryItems;
+    }
+    
+    /**
+     * Extended GitHub Content Service, adds ability to retrieve the repo archive  
+     */
     public static class ContentsServiceEx extends ContentsService
     {
 		public ContentsServiceEx(GitHubClient client) {
@@ -169,8 +231,6 @@ public class GitHubSalesforceDeployController {
      * Adds support for OAuth Client ID and Client Secret authentication (server to server)
      * 
      * Note: Only overrides 'get' and 'getStream'
-     * 
-     * @author afawcett
      */
     public static class GitHubClientOAuthServer extends GitHubClient
     {
@@ -206,15 +266,41 @@ public class GitHubSalesforceDeployController {
     }
     
     /**
+     * Discovers the contents of a GitHub repository
+     * @param contentService
+     * @param repoId
+     * @param contents
+     * @param repositoryContainer
+     * @throws Exception
+     */
+    private static void processContents(ContentsService contentService, RepositoryId repoId, List<RepositoryContents> contents, RepositoryItem repositoryContainer)
+    		throws Exception
+	{
+    	for(RepositoryContents repo : contents)
+    	{
+			RepositoryItem repositoryItem = new RepositoryItem();
+			repositoryItem.repositoryItem = repo;
+			repositoryContainer.repositoryItems.add(repositoryItem);
+    		if(repo.getType().equals("dir"))
+    		{
+    			repositoryItem.repositoryItems = new ArrayList<RepositoryItem>();
+    			processContents(contentService, repoId, contentService.getContents(repoId, repo.getPath()), repositoryItem);
+    		}
+    	}
+	}
+    
+    /**
      * Print out any errors, if any, related to the deploy.
      * @param result - DeployResult
      */
     private static String printErrors(DeployResult result)
     {
         DeployMessage messages[] = result.getMessages();
-        StringBuilder buf = new StringBuilder("Failures:\n");
+        StringBuilder buf = new StringBuilder();
         for (DeployMessage message : messages) {
             if (!message.isSuccess()) {
+            	if(buf.length()==0)
+            		buf = new StringBuilder("Failures:\n");
                 String loc = (message.getLineNumber() == 0 ? "" :
                     ("(" + message.getLineNumber() + "," +
                             message.getColumnNumber() + ")"));
--- src/main/webapp/WEB-INF/jsp/githubdeploy.jsp
+++ src/main/webapp/WEB-INF/jsp/githubdeploy.jsp
@@ -3,7 +3,148 @@
 <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <jsp:include page="header.jsp"/>
 	</p>
-	<pre>${deploymentFiles}</pre>
-	<pre>${deployProgress}</pre>
-	<pre>${resultErrorMessages}</pre>
-<jsp:include page="footer.jsp"/>
+    <c:if test="${error != null}">
+        <div class="alert">${error}</div>
+    </c:if>	
+    <table class="table table-striped table-condensed">
+        <tr>
+            <td>Repository Name:</td>
+            <td><c:out value="${repositoryName}"/></td>
+        </tr>
+	    <c:if test="${repo != null}">
+	        <tr>
+	            <td>Repository Description:</td>
+	            <td>${repo.getDescription()}</td>
+	        </tr>
+	        <tr>
+	            <td>Repository URL:</td>
+	            <td><a href="${repo.getHtmlUrl()}" target="_new">Repository</td>
+	        </tr>
+	    </c:if>
+        <tr>
+            <td>Salesforce Organization Name:</td>
+            <td><c:out value="${userContext.getOrganizationName()}"/></td>
+        </tr>
+        <tr>
+            <td>Salesforce User Name:</td>
+            <td><c:out value="${userContext.getUserName()}"/></td>
+        </tr>
+    </table>    
+    <c:if test="${githubcontents != null}">
+		<pre id="githubcontents"></pre>		
+	    <div class="btn-group">
+	        <input id="deploy" value="Deploy" type="button" onclick="GitHubDeploy.deploy();" class="btn"/>
+	    </div>	    
+	    <pre id="deploystatus" style="display:none">
+	    </pre>
+    </c:if>			
+</div>
+<script src="/resources/js/jquery-1.7.1.min.js"></script>
+<c:if test="${githubcontents != null}">
+	<script type="text/javascript">
+	
+		var GitHubDeploy = { 
+		
+			// Contents of the GitHub repository
+			contents: ${githubcontents},
+			
+			// Async result from Salesforce Metadata API
+			asyncResult : null,
+			
+			// Client timer Id used to poll Salesforce Metadata API
+			intervalId : null,
+			
+			// Render GitHub repository contents
+			render: function(container) {
+					if(container.repositoryItem!=null)
+						$('#githubcontents').append('<div>' + container.repositoryItem.path + '</div>');
+					for(fileIdx in container.repositoryItems)
+						if(container.repositoryItems[fileIdx].repositoryItem.type == 'dir')
+							GitHubDeploy.render(container.repositoryItems[fileIdx]);
+						else
+							$('#githubcontents').append('<div>' + container.repositoryItems[fileIdx].repositoryItem.path + '</div>');
+				},
+				
+			// Deploy
+			deploy: function() {
+					$('#deploy').attr('disabled', 'disabled');
+					$('#deploystatus').empty();
+					$('#deploystatus').show();
+					$('#deploystatus').append('Deployment Started');
+		            $.ajax({
+		                type: 'POST',
+		                processData : false,
+		                data : JSON.stringify(GitHubDeploy.contents),
+		                contentType : "application/json; charset=utf-8",
+		                dataType : "json",
+		                success: function(data, textStatus, jqXHR) {
+		                    GitHubDeploy.asyncResult = data;
+		                    GitHubDeploy.renderAsync();
+		                    if(GitHubDeploy.asyncResult.state == 'Completed')
+		                    	GitHubDeploy.checkDeploy();
+		                    else
+		                    	GitHubDeploy.intervalId = window.setInterval(GitHubDeploy.checkStatus, 2000);
+		                },
+		                error: function(jqXHR, textStatus, errorThrown) {
+		                    alert('Failed ' + textStatus + errorThrown);
+		                }
+		            });			
+				},
+				
+			// Render Async
+			renderAsync: function() {
+					$('#deploystatus').append(
+						'<div>Status: '+ 
+							GitHubDeploy.asyncResult.state + ' ' + 
+							(GitHubDeploy.asyncResult.message != null ? GitHubDeploy.asyncResult.message : '') +
+						'</div>');
+				},
+				
+			// Check Status
+			checkStatus: function() {
+		            $.ajax({
+		                type: 'GET',
+		                url: window.location + '/checkstatus/' + GitHubDeploy.asyncResult.id,
+		                contentType : 'application/json; charset=utf-8',
+		                dataType : 'json',
+		                success: function(data, textStatus, jqXHR) {
+		                    GitHubDeploy.asyncResult = data;
+		                    GitHubDeploy.renderAsync();
+		                    if(GitHubDeploy.asyncResult.state == 'Completed')
+		                    {
+		                    	window.clearInterval(GitHubDeploy.intervalId);
+		                    	GitHubDeploy.checkDeploy();
+		                    }
+		                },
+		                error: function(jqXHR, textStatus, errorThrown) {
+		                	$('#deploystatus').append('<div>Error: ' + textStatus + errorThrown + '</div>');
+		                }
+		            });					
+				},
+			
+			// Check Deploy
+			checkDeploy: function() {
+					$('#deploystatus').append('Deployment Complete');		
+					$('#deploy').attr('disabled', null);
+		            $.ajax({
+		                type: 'GET',
+		                url: window.location + '/checkdeploy/' + GitHubDeploy.asyncResult.id,
+		                contentType : 'application/json; charset=utf-8',
+		                dataType : 'json',
+		                success: function(data, textStatus, jqXHR) {
+		                	$('#deploystatus').append(data);
+		                },
+		                error: function(jqXHR, textStatus, errorThrown) {
+		                	$('#deploystatus').append('<div>Error: ' + textStatus + errorThrown + '</div>');
+		                }
+		            });													
+				}
+		}
+		
+		// Render files selected to deploy
+		GitHubDeploy.render(GitHubDeploy.contents);
+		
+	</script>
+</c:if>	
+</body>
+</html>
